319b2eddf05d3835289659c3a33bc5aa1824e466
Patricia Christin Coberger <pcoberge@mail.uni-mannheim.de>
2019-03-11 18:34:18 +0100

    #12 Edit Class Continent

--- src/game/Continent.java
+++ src/game/Continent.java
@@ -1,5 +1,27 @@
 package game;
 
-public class Continent {
+import java.util.HashSet;
 
+public class Continent {
+  private HashSet<Territory> territories;
+  private Continente name;
+  private int value;
+  
+  public Continent(Continente name, int value, HashSet<Territory> territories) {
+    this.name = name;
+    this.value = value;
+    this.territories = territories;
+  }
+
+  public HashSet<Territory> getTerritories() {
+    return territories;
+  }
+
+  public Continente getName() {
+    return name;
+  }
+
+  public int getValue() {
+    return value;
+  }  
 }
