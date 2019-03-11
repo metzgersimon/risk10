ef10ab8e6eaef2ae094662278c61952f94455895
Liangda Wang <liwang@mail.uni-mannheim.de>
2019-03-11 23:51:25 +0100

    #19 main methode modified to open FX UI LoginPanel

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,32 @@
 package main;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
-public class Main {
+/*
+  * @author liwang
+ * main methode to open fx UI LoginPanel
+ *
+ */
 
-	public static void main(String[] args) {
-
-		
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/gui/LoginPanel.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("/gui/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
