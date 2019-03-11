135fc458c3e12ebc4b1c2d7ba3e4ebce72d95a10
Preston To <prto@mail.uni-mannheim.de>
2019-03-11 17:29:20 +0100

    #7 further restructuring

--- src/main/main.java
+++ src/main/main.java
@@ -1,8 +1,8 @@
-package game;
+package main;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void Main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
