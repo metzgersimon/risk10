5996063bfefa4abfa2f0dbb6365be3d7d390a353
Patricia Christin Coberger <pcoberge@mail.uni-mannheim.de>
2019-03-11 18:38:00 +0100

    #15 Create Enum CardSymbol

---
+++ src/game/CardSymbol.java
@@ -0,0 +1,9 @@
+package game;
+/**
+ * @author pcoberge
+ * Enum defines symbols of risk-cards.
+ * It is used for risk-cards and as symbols of each country.
+ */
+public enum CardSymbol {
+  INFANTRY, CANNON, CAVALRY;
+}
