3969f53a765cd0133e5924c3cb973d55e5611cf3
Preston To <prto@mail.uni-mannheim.de>
2019-03-20 23:59:44 +0100

    #33 created and uploaded avatars

--- src/gui/ProfileManager.java
+++ src/gui/ProfileManager.java
@@ -13,73 +13,73 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author prto
- * Class to manage multiple player profiles
+ * @author prto Class to manage multiple player profiles
  */
 
 public class ProfileManager {
-  public static HashMap<String,PlayerProfile> profileList;
+  public static HashMap<String, PlayerProfile> profileList;
   PlayerProfile selectedProfile;
-  
-  //Add new profile to profileList
+
+  // Add new profile to profileList
   public void addNewProfile(String name, int imageID) {
     readXML();
-    PlayerProfile temp = new PlayerProfile(name,imageID);
-    profileList.put(name,temp);
+    PlayerProfile temp = new PlayerProfile(name, imageID);
+    profileList.put(name, temp);
   }
-  
+
   public void setSelectedProfile() {
-    
+
   }
-  
+
   public PlayerProfile getSelectedProfile() {
     return this.selectedProfile;
   }
-  
 
-  //Loads all player profiles from PlayerProfiles.xml file
+
+  // Loads all player profiles from PlayerProfiles.xml file
   public void readXML() {
     SAXBuilder builder = new SAXBuilder();
     File xml = new File("PlayerProfiles.xml");
-    
+
     try {
       Document doc = (Document) builder.build(xml);
       Element root = doc.getRootElement();
       List<Element> profiles = root.getChildren();
-      profileList = new HashMap<String,PlayerProfile>();
-      
-      for(int i = 0; i < profiles.size(); i++) {
-        
+      profileList = new HashMap<String, PlayerProfile>();
+
+      for (int i = 0; i < profiles.size(); i++) {
+
         Element profile = (Element) profiles.get(i);
-        
+
         String name = profile.getChildText("name");
         int imageID = Integer.parseInt(profile.getChildText("imageID"));
         int matchesPlayed = Integer.parseInt(profile.getChildText("matchesPlayed"));
         int matchesWon = Integer.parseInt(profile.getChildText("matchesWon"));
         int matchesLost = Integer.parseInt(profile.getChildText("matchesLost"));
         int territoriesConquered = Integer.parseInt(profile.getChildText("territoriesConquered"));
-        
-        PlayerProfile temp = new PlayerProfile(name, imageID, matchesPlayed, matchesWon, matchesLost, territoriesConquered);
+
+        PlayerProfile temp = new PlayerProfile(name, imageID, matchesPlayed, matchesWon,
+            matchesLost, territoriesConquered);
         profileList.put(name, temp);
       }
-      
-    } catch(IOException ioe) {
+
+    } catch (IOException ioe) {
       System.out.println(ioe.getMessage());
-    } catch(JDOMException jde) {
+    } catch (JDOMException jde) {
       System.out.println(jde.getMessage());
     }
   }
-  
-  //Saves profileList to xml
+
+  // Saves profileList to xml
   public void saveXML() {
     try {
       Element root = new Element("risk10");
       Document doc = new Document(root);
       doc.setRootElement(root);
-      Element player; 
-      
-      //Write elements
-      for(PlayerProfile x : profileList.values()) {
+      Element player;
+
+      // Write elements
+      for (PlayerProfile x : profileList.values()) {
         player = new Element("Player");
         player.addContent(new Element("name").setText(x.getName()));
         player.addContent(new Element("imageID").setText(x.getID()));
@@ -88,15 +88,15 @@ public class ProfileManager {
         player.addContent(new Element("matchesLost").setText(x.getMatchesLost()));
         player.addContent(new Element("territoriesConquered").setText(x.getTerritoriesConquered()));
       }
-      
-      //Save to xml
+
+      // Save to xml
       XMLOutputter xml = new XMLOutputter();
       xml.setFormat(Format.getPrettyFormat());
-      xml.output(doc,new FileWriter("PlayerProfiles.xml"));
-      
-    } catch(IOException ioe) {
+      xml.output(doc, new FileWriter("PlayerProfiles.xml"));
+
+    } catch (IOException ioe) {
       System.out.println(ioe.getMessage());
     }
   }
-  
+
 }
