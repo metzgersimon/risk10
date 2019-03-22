e5737e80739a8d37e83f6931578c6ee0e62ea889
Liangda Wang <liwang@mail.uni-mannheim.de>
2019-03-22 17:21:33 +0100

    #20 more gui and controller class added

---
+++ src/gui/CreateProfileGUIController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CreateProfileGUIController {
+
+    @FXML
+    private Button conform;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    void conform(ActionEvent event) {
+      try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProfileSelectionGUI.fxml"));
+        BorderPane root = (BorderPane) fxmlLoader.load();
+        Stage stage = new Stage();
+        
+        Label username  = new Label();
+        username.setText(name.getText());
+        
+        
+        root.getChildren().add(username);
+      // root.getChildren().add(name);
+        
+        stage.setTitle("Profile Selection");
+        stage.setScene(new Scene(root));
+        stage.show();
+        ((Node) event.getSource()).getScene().getWindow().hide();
+      } catch (Exception e) {
+        System.out.println("Can't load ProfileSelectionGUI.fxml");
+      }
+    }
+
+}
---
+++ src/gui/MultiPlayerGUIController.java
@@ -0,0 +1,60 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MultiPlayerGUIController {
+
+    @FXML
+    private Button hostGame;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button joinGame;
+
+    @FXML
+    void back(ActionEvent event) {
+      try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProfileSelectionGUI.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Profile Selection");
+        stage.setScene(new Scene(root));
+        stage.show();
+        ((Node) event.getSource()).getScene().getWindow().hide();
+      } catch (Exception e) {
+        System.out.println("Can't load ProfileSelectionGUI.fxml");
+      }
+    }
+
+    @FXML
+    void hostGame(ActionEvent event) {
+      try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HostGameLobby.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Host Game");
+        stage.setScene(new Scene(root));
+        stage.show();
+        ((Node) event.getSource()).getScene().getWindow().hide();
+      } catch (Exception e) {
+        System.out.println("Can't load HostGameLobby.fxml");
+        e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void joinGame(ActionEvent event) {
+      
+    }
+
+}
+
