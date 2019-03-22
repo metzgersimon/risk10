58110dde8ebbb2caeb430dcc381fb7425300bc55
Liangda Wang <liwang@mail.uni-mannheim.de>
2019-03-22 22:02:43 +0100

    #20 able to choose profile image

--- src/gui/CreateProfileGUIController.java
+++ src/gui/CreateProfileGUIController.java
@@ -1,15 +1,22 @@
 package gui;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class CreateProfileGUIController {
@@ -19,6 +26,31 @@ public class CreateProfileGUIController {
 
   @FXML
   private TextField name;
+
+  @FXML
+  private ImageView profileImage;
+
+  @FXML
+  void chooseImage(MouseEvent event) {
+    FileChooser fileChooser = new FileChooser();
+    File file = fileChooser.showOpenDialog(null);
+
+    FileChooser.ExtensionFilter jpg = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+    fileChooser.getExtensionFilters().add(jpg);
+    fileChooser.setTitle("Select a profile image");
+    fileChooser.setInitialDirectory(
+        new File(System.getProperty("user.home"), ".risk10/resources/avatars"));
+
+    try {
+      BufferedImage bufferedImage = ImageIO.read(file);
+      Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+      profileImage.setImage(image);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
 
   @FXML
   void conform(ActionEvent event) {
@@ -33,6 +65,8 @@ public class CreateProfileGUIController {
 
       root.getChildren().add(username);
       // root.getChildren().add(name);
+      
+      
 
       stage.setTitle("Profile Selection");
       stage.setScene(new Scene(root));
