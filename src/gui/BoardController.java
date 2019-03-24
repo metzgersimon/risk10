bf166e9aa71e1262ab23eeaa0dbd70928d45e79a
Patricia Christin Coberger <pcoberge@mail.uni-mannheim.de>
2019-03-24 11:07:08 +0100

    #23 edit action methods regarding to interactive world map and connect Territories, Regions and Labels.

--- src/gui/BoardController.java
+++ src/gui/BoardController.java
@@ -54,66 +54,83 @@ public class BoardController {
   @FXML
   private SplitPane splitter;
 
-  
+
   /**
    * Elements that handle leave option
    */
-  @FXML private Shape circleLeave;
-  @FXML private ImageView imageLeave;
-  @FXML private Pane quitPane;
-  @FXML private Pane grayPane;
-  @FXML private Button yesLeave;
-  @FXML private Button noLeave;
-  
+  @FXML
+  private Shape circleLeave;
+  @FXML
+  private ImageView imageLeave;
+  @FXML
+  private Pane quitPane;
+  @FXML
+  private Pane grayPane;
+  @FXML
+  private Button yesLeave;
+  @FXML
+  private Button noLeave;
+
   /**
-   * Elements that show the current game state and
-   * illustrate who is the current player
+   * Elements that show the current game state and illustrate who is the current player
    */
-  @FXML private ProgressBar progress;
-  
+  @FXML
+  private ProgressBar progress;
+
   /**
    * Elements to handle the card split-pane
    */
-  @FXML private SplitPane cardPane;
-  @FXML private Button upAndDown;
-  
+  @FXML
+  private SplitPane cardPane;
+  @FXML
+  private Button upAndDown;
+
   /**
    * Elements to handle trade-in button
    */
-  @FXML private Button tradeIn;
-  @FXML private Label tradedCards;
+  @FXML
+  private Button tradeIn;
+  @FXML
+  private Label tradedCards;
 
   /**
    * Element which handles the function to skip a game state
    * 
    */
-  @FXML private ImageView skip;
-  @FXML private Label gameState;
-  
+  @FXML
+  private ImageView skip;
+  @FXML
+  private Label gameState;
+
   public BoardGUI_Main boardGui;
 
   public void setMain(BoardGUI_Main boardGui) {
     this.boardGui = boardGui;
   }
 
-  
-  //############################################
+
+  // ############################################
   /**
-   * @param e
+   * @author pcoberge
+   * This method handles the exit Button. When the user presses the exit-Button, a pop-up window appears and stops the game.
    */
   public void pressLeave() {
     this.grayPane.toFront();
     this.quitPane.toFront();
   }
-  
+
+  /**
+   * @author pcoberge
+   * This method cancels the exit-handling.
+   */
   public void handleNoLeave() {
     this.grayPane.toBack();
     this.quitPane.toBack();
   }
-  
+
   /**
-   * @param event : ActionEvent
-   * This parameter represents the element that invokes this method.
+   * @author pcoberge
+   * @param event : ActionEvent This parameter represents the element that invokes this method.
    * This action method changes the current stage to the statistic stage.
    */
   public void handleLeave(ActionEvent event) {
@@ -124,24 +141,24 @@ public class BoardController {
       stage.setTitle("Statistics");
       stage.setScene(new Scene(root));
       stage.show();
-     //((Node) event.getSource()).getScene().getWindow().hide();
+      // ((Node) event.getSource()).getScene().getWindow().hide();
     } catch (Exception e) {
       System.out.println("Can't load StatisticGUI.fxml");
     }
   }
-  
+
   /**
    * This method is a dummy Method, to get to know, how the ProgressBAr can be handled
    */
   public void handleProgressBar() {
-    //String color = p.getColor().toString().toLowerCase();
-   // this.progress.setStyle("-fx-accent: "+color+";");
-//    this.progress.setProgress(0.4);
+    // String color = p.getColor().toString().toLowerCase();
+    // this.progress.setStyle("-fx-accent: "+color+";");
+    // this.progress.setProgress(0.4);
   }
-  
-  //#############################################
-  
-  
+
+  // #############################################
+
+
   /**
    * 
    */
@@ -152,28 +169,28 @@ public class BoardController {
     Button button = new Button();
     button.textProperty().bind(Bindings.when(collapsed).then("V").otherwise("^"));
     button.setOnAction(f -> {
-      double target = collapsed.get() ? 0.4: 0.1;
-      KeyValue kv = new KeyValue(cardPane.getDividers().get(0).positionProperty(),target);
+      double target = collapsed.get() ? 0.4 : 0.1;
+      KeyValue kv = new KeyValue(cardPane.getDividers().get(0).positionProperty(), target);
       Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500), kv));
       timeline.play();
     });
-    
+
   }
-  
+
   /**
    * 
    */
   @FXML
   public void handleTradeCards(ActionEvent e) {
     int i = 0;
-    if(e.getSource().equals(tradeIn)) {
+    if (e.getSource().equals(tradeIn)) {
       tradedCards = new Label("traded card sets:" + i);
       i++;
-       
+
       System.out.println("Buttong geklickt");
     }
   }
-  
+
   /**
    * 
    */
@@ -181,25 +198,26 @@ public class BoardController {
   public void handleSkipGameState() {
     handleProgressBar();
     this.skip.setOnMouseClicked(new EventHandler<MouseEvent>() {
-    double i = 0.2;
+      double i = 0.2;
+
       @Override
       public void handle(MouseEvent e) {
-//        progress.setProgress(0);
+        // progress.setProgress(0);
         // switch(game.getGameState()) {
         // case PLACE_ARMIES:
-        if(progress.getProgress() < 0.8) {
-          progress.setProgress(progress.getProgress()+i);
+        if (progress.getProgress() < 0.8) {
+          progress.setProgress(progress.getProgress() + i);
         }
-//        progress.setProgress(0.3);
-//        gameState = new Label("ATTACKING");
+        // progress.setProgress(0.3);
+        // gameState = new Label("ATTACKING");
         // break;
         // case ATTACKING:
-//        progress.setProgress(0.6);
-//        gameState = new Label("FORTIFY");
+        // progress.setProgress(0.6);
+        // gameState = new Label("FORTIFY");
         // break;
         // case FORTIFY:
-//        progress.setProgress(1);
-//        gameState = new Label("END");
+        // progress.setProgress(1);
+        // gameState = new Label("END");
         // break;
         // }
         System.out.println("IMAGE GEKLICKT");
@@ -209,8 +227,8 @@ public class BoardController {
 
   }
 
-  
-  
+
+
   @FXML
   public void handleClickCRB() {
     if (chatRoomButton.getText().equals("<")) {
@@ -227,69 +245,99 @@ public class BoardController {
     System.out.println("TEST");
   }
 
-//  @FXML
-//  public void clicked(MouseEvent e) {
-//    Player p = null;
-//    Region r = (Region) e.getSource();
-//    Territory territory = game.getWorld().getTerritoriesRegion().get(r);
-//    switch (game.getState()) {
-//      case INITIALISING:
-//        /**
-//         * if a player selects a territory, he becomes the owner of this territory and places his
-//         * army at this territory
-//         */
-//        for (Territory t : game.getWorld().getTerritories().values()) {
-//          t.getRegion().setEffect(new Glow(0.0));
-//        }
-//        r.setEffect(new Glow(0.0));
-//        territory.setOwner(p);
-//        territory.setNumberOfArmies(1);
-//        break;
-//      case ARMY_REINFORCEMENT:
-//        // Methode mit Eingabe der Anzahl der Armeen
-//        break;
-//      case ATTACKING:
-//        /**
-//         * if a player selects a territory he owns, all territories that aren't neighbors and belong
-//         * to him will be disabled. if a player selects a territory his selected territory is
-//         * neighbor of,
-//         */
-//        if (selectedTerritory == null) {
-//          r.setEffect(new Glow(0.3));
-//          selectedTerritory = territory;
-//          for (Territory t : game.getWorld().getTerritories().values()) {
-//            if (!t.equals(territory) && !(territory.getNeighbor().contains(t))) {
-//              t.getRegion().setDisable(true);
-//            }
-//          }
-//        } else {
-//          /*
-//           * Boolean winner = attack(selectedTerritory, territory, numberOfArmies); Pop-Up Eingabe
-//           * mit Anzahl der Armeen if (winner) { r.getShape().setFill(p.color);
-//           * territory.setOwner(p); //Pop-UP Eingabe mit Anzahl der Armeen
-//           * territory.setNumberOfArmies(amount); selectedTerritory.setNumberOfArmies(-amount);
-//           */
-//        }
-//        selectedTerritory = null;
-//        r.setEffect(new Glow(0.0));
-//        selectedTerritory.getRegion().setEffect(new Glow(0.0));
-//        break;
-//    }
-//  }
+  // @FXML
+  // public void clicked(MouseEvent e) {
+  // Player p = null;
+  // Region r = (Region) e.getSource();
+  // Territory territory = game.getWorld().getTerritoriesRegion().get(r);
+  // switch (game.getState()) {
+  // case INITIALISING:
+  // /**
+  // * if a player selects a territory, he becomes the owner of this territory and places his
+  // * army at this territory
+  // */
+  // for (Territory t : game.getWorld().getTerritories().values()) {
+  // t.getRegion().setEffect(new Glow(0.0));
+  // }
+  // r.setEffect(new Glow(0.0));
+  // territory.setOwner(p);
+  // territory.setNumberOfArmies(1);
+  // break;
+  // case ARMY_REINFORCEMENT:
+  // // Methode mit Eingabe der Anzahl der Armeen
+  // break;
+  // case ATTACKING:
+  // /**
+  // * if a player selects a territory he owns, all territories that aren't neighbors and belong
+  // * to him will be disabled. if a player selects a territory his selected territory is
+  // * neighbor of,
+  // */
+  // if (selectedTerritory == null) {
+  // r.setEffect(new Glow(0.3));
+  // selectedTerritory = territory;
+  // for (Territory t : game.getWorld().getTerritories().values()) {
+  // if (!t.equals(territory) && !(territory.getNeighbor().contains(t))) {
+  // t.getRegion().setDisable(true);
+  // }
+  // }
+  // } else {
+  // /*
+  // * Boolean winner = attack(selectedTerritory, territory, numberOfArmies); Pop-Up Eingabe
+  // * mit Anzahl der Armeen if (winner) { r.getShape().setFill(p.color);
+  // * territory.setOwner(p); //Pop-UP Eingabe mit Anzahl der Armeen
+  // * territory.setNumberOfArmies(amount); selectedTerritory.setNumberOfArmies(-amount);
+  // */
+  // }
+  // selectedTerritory = null;
+  // r.setEffect(new Glow(0.0));
+  // selectedTerritory.getRegion().setEffect(new Glow(0.0));
+  // break;
+  // }
+  // }
 
+  /**
+   * @author pcoberge
+   * @param e
+   * This action method highlights the current region, when the mouse enters.
+   */
   @FXML
   public void motionIn(MouseEvent e) {
-    Region r = (Region) e.getSource();
-    if (r.getEffect() == null) {
-      r.setEffect(new Glow(0.2));
+    if (e.getSource() instanceof Label) {
+      Label l = (Label) e.getSource();
+      Region r = Game.getWorld().getTerritoriesName().containsKey(l)
+          ? Game.getWorld().getTerritoriesName().get(l).getBoardRegion().getRegion()
+          : Game.getWorld().getTerritoriesNoA().get(l).getBoardRegion().getRegion();
+      if (r.getEffect() == null) {
+        r.setEffect(new Glow(0.2));
+      }
+    } else {
+      Region r = (Region) e.getSource();
+      if (r.getEffect() == null) {
+        r.setEffect(new Glow(0.2));
+      }
     }
   }
-  
+
+  /**
+   * @author pcoberge
+   * @param e
+   * This action method deletes the highlight the current region is affected with, when the mouse exits. 
+   */
   @FXML
   public void motionOut(MouseEvent e) {
-    Region r = (Region) e.getSource();
-    if (r.getEffect() != null) {
-      r.setEffect(null);
+    if (e.getSource() instanceof Label) {
+      Label l = (Label) e.getSource();
+      Region r = Game.getWorld().getTerritoriesName().containsKey(l)
+          ? Game.getWorld().getTerritoriesName().get(l).getBoardRegion().getRegion()
+          : Game.getWorld().getTerritoriesNoA().get(l).getBoardRegion().getRegion();
+      if (r.getEffect() != null) {
+        r.setEffect(null);
+      }
+    } else {
+      Region r = (Region) e.getSource();
+      if (r.getEffect() != null) {
+        r.setEffect(null);
+      }
     }
   }
 }
---
+++ src/gui/BoardRegion.java
@@ -0,0 +1,35 @@
+package gui;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+
+public class BoardRegion {
+  private Region territory;
+  private Label headline;
+  private Label numberOfArmy;
+  
+  public BoardRegion(Region t, Label h, Label n) {
+    this.territory = t;
+    this.headline = h;
+    this.numberOfArmy = n;
+  }
+  
+  public Region getRegion() {
+    return territory;
+  }
+  
+  public Label getHeadline() {
+    return this.headline;
+  }
+  
+  public Label getNumberOfArmy() {
+    return this.numberOfArmy;
+  }
+  
+  public void setNumberOfArmy(int amount) {
+    int current = Integer.parseInt(this.numberOfArmy.getText());
+    int erg = current + amount;
+    this.numberOfArmy.setText(erg + "");
+  }
+  
+}
