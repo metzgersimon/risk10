ee9a3a16a2d3ca8488ba909e3ffc233c5b0e4e1a
Simon Metzger <smetzger@mail.uni-mannheim.de>
2019-04-02 16:43:16 +0200

    #8 modified dice methods

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -2,6 +2,8 @@ package game;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Vector;
 import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.SnapshotParameters;
@@ -139,7 +141,7 @@ public class Dice {
    * 
    * @return an int number which is calculated randomly
    */
-  public int rollSingleDice() {
+  public static int rollSingleDice() {
     return (int) ((Math.random() * 6 + 1));
   }
 
@@ -151,11 +153,12 @@ public class Dice {
    * @return the rolled values from
    */
 
-  public int[] rollDices(int numberOfDices) {
-    int[] rolledNumbers = new int[numberOfDices];
-    for (int i = 0; i < rolledNumbers.length; i++) {
-      rolledNumbers[i] = rollSingleDice();
+  public static Vector<Integer> rollDices(int numberOfDices) {
+    Vector<Integer> rolledNumbers = new Vector<Integer>();
+    for (int i = 0; i < numberOfDices; i++) {
+     rolledNumbers.add(rollSingleDice());
     }
+    Collections.sort(rolledNumbers);
     return rolledNumbers;
   }
 
