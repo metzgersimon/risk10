fda74c3fdf41344d972f5dffb1d72652f092ebc7
Qiyu Chen <qiychen@mail.uni-mannheim.de>
2019-04-04 16:06:33 +0200

    #22 add method to clients

--- src/network/client/Client.java
+++ src/network/client/Client.java
@@ -3,32 +3,113 @@ package network.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import game.Player;
 import network.Parameter;
+import network.messages.JoinGameMessage;
+import network.messages.Message;
 
 public class Client extends Thread {
 
   private InetAddress address;
   private Socket s;
+  private Player player;
+  private ObjectInputStream fromServer;
+  private ObjectOutputStream toServer;
+  private boolean active;
 
   public Client(InetAddress address) {
-  this.address = address;
-  connect();
+    this.address = address;
+    connect();
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param address
+   * @param player
+   */
+  public Client(InetAddress address, Player player) {
+    this.address = address;
+    this.player = player;
+    connect();
   }
 
   public void connect() {
     try {
       this.s = new Socket(address, Parameter.PORT);
+      fromServer = new ObjectInputStream(s.getInputStream());
+      toServer = new ObjectOutputStream(s.getOutputStream());
+      this.active = true;
       System.out.println("Socket opened successfully");
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
-public void run() {
-  
-}
+
+  public void run() {
+
+  }
+
+  /**
+   * send message to server
+   * 
+   * @param m
+   */
+  public void sendMessage(Message m) {
+    try {
+      toServer.writeObject(m);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  public void register() {
+    JoinGameMessage join = new JoinGameMessage(player);
+    this.sendMessage(join);
+    // To get response
+  }
+
+  public void transact() {
+    while (active) {
+      try {
+        Message message = (Message) fromServer.readObject();
+        switch (message.getType()) {
+          case LEAVE:
+            break;
+          case DISPLAY:
+            break;
+        }
+      } catch (ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+
+  /**
+   * disconnect the conneciton
+   */
+  public void disconnect() {
+    try {
+      active = false;
+      this.fromServer.close();
+      this.toServer.close();
+      s.close();
+      System.out.println("Protocol ended");
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
 }
 
--- src/network/messages/JoinGameMessage.java
+++ src/network/messages/JoinGameMessage.java
@@ -1,11 +1,24 @@
 package network.messages;
 
+import game.Player;
+
 public class JoinGameMessage extends Message {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
+  private Player player;
 
-	public JoinGameMessage(){
-		super(MessageType.JOIN);
-	}
+  public JoinGameMessage(Player player) {
+    super(MessageType.JOIN);
+    this.player = player;
+
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
 
 }
