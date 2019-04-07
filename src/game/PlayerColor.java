bf68c8ac757a28d877e5da55ee1a4ac6d98736f8
Simon Metzger <smetzger@mail.uni-mannheim.de>
2019-04-07 21:10:45 +0200

    #11 changed player colors to make the labels more readable and the colors more distinguishable

--- src/game/PlayerColor.java
+++ src/game/PlayerColor.java
@@ -7,7 +7,7 @@ import javafx.scene.paint.Color;
  * @author smetzger Enum defines color for a player during the game
  */
 public enum PlayerColor {
-  YELLOW, BLUE, RED, GREEN, MAGENTA, WHITE;
+  YELLOW, BLUE, RED, GREEN, MAGENTA, ORANGE;
 
 
   public String getRgbColor() {
@@ -24,7 +24,7 @@ public enum PlayerColor {
       return "rgb(0, 0, 255)";
     case MAGENTA:
       return "rgb(255, 0, 255)";
-    case WHITE:
+    case ORANGE:
       return "rgb(255, 165, 0)";
     default:
       return null;
@@ -38,13 +38,13 @@ public enum PlayerColor {
       case RED:
         return Color.RED;
       case GREEN:
-        return Color.GREEN;
+        return Color.LAWNGREEN;
       case MAGENTA:
         return Color.MAGENTA;
       case BLUE:
-        return Color.BLUE;
-      case WHITE:
-        return Color.WHITE;
+        return Color.DODGERBLUE;
+      case ORANGE:
+        return Color.ORANGE;
       default:
         return Color.BLACK;
     }
