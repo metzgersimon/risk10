173464690fe4acc43a8fa341771790b899fa89c1
Preston To <prto@mail.uni-mannheim.de>
2019-04-10 13:06:28 +0200

    #27 created TutorialMessages class, starting to create messages

---
+++ src/game/TutorialMessages.java
@@ -0,0 +1,28 @@
+package game;
+
+/**
+ * 
+ * @author Preston
+ *
+ */
+
+public class TutorialMessages {
+
+  public static String welcome = "Welcome to Tutorial Mode";
+  public static String intro = "This is the game board. Interaction will mainly be done through clicking on the territories.";
+  public static String distributing = "You are now in the distributing phase. Players take turns distributing their armies across the map.";
+  public static String distributingTip = "";
+  public static String attacking1 = "You are now in the attacking phase. Click one of your territories to start";
+  public static String attacking2 = "Now pick an adjacent territory which belongs to an opponent to attack";
+  public static String dicing = "You now have to choose how many armies you want to attack with. You then roll dices against your chosen opponent";
+  public static String attackingTip = "";
+  public static String attackSuccess = "Your attack was a success! You can now attack again or end your round";
+  public static String attackFailed = "";
+  public static String defending = "An opponent has chosen to attack your territory. Roll the dice to defend it.";
+  public static String defendingTip = "";
+  public static String fortify = "";
+  public static String fortifyTip = "";
+  public static String tradeIn = "";
+  public static String tradeInTip = "";
+  
+}
