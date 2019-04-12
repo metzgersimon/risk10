b6253a6df74fa2b26fe14a2539e7cd4b6f21d98a
Preston To <prto@mail.uni-mannheim.de>
2019-04-13 00:41:20 +0200

    #27 added more tutorial tips

--- src/game/TutorialMessages.java
+++ src/game/TutorialMessages.java
@@ -9,20 +9,23 @@ package game;
 public class TutorialMessages {
 
   public static String welcome = "Welcome to Tutorial Mode";
-  public static String intro = "This is the game board. Interaction will mainly be done through clicking on the territories.";
-  public static String distributing = "You are now in the distributing phase. Players take turns distributing their armies across the map.";
-  public static String distributingTip = "";
+  public static String intro = "This is the game board. Interaction will mainly be done through clicking on the territories";
+  public static String distributing = "You are now in the distributing phase. Players take turns distributing their armies across the map";
+  public static String distributingTip = "Tip: Isolated continents are easier to maintain, but are also more difficult to expand from";
   public static String attacking1 = "You are now in the attacking phase. Click one of your territories to start";
   public static String attacking2 = "Now pick an adjacent territory which belongs to an opponent to attack";
   public static String dicing = "You now have to choose how many armies you want to attack with. You then roll dices against your chosen opponent";
-  public static String attackingTip = "";
-  public static String attackSuccess = "Your attack was a success! You can now attack again or end your round";
-  public static String attackFailed = "";
+  public static String attackingTip = "Tip: Your chances of winning an attack are higher if you attack a territory with fewer armies than your attacking territory";
+  public static String attackSuccess = "Your attack was a success! Your opponent lost armies. You can attack again or pass";
+  public static String attackFailed = "Your attack failed! You lost armies. You can attack again or pass";
   public static String defending = "An opponent has chosen to attack your territory. Roll the dice to defend it.";
-  public static String defendingTip = "";
-  public static String fortify = "";
-  public static String fortifyTip = "";
-  public static String tradeIn = "";
+  public static String defendingTip = "Keep track of the amount of enemy armies adjacent to your territories: they might be planning an attack soon";
+  public static String fortify = "You can now move armies between adjacent territories";
+  public static String fortifyTip = "Tip: Decrease the chances of weak spots by fortifying your border territories against opponent attacks";
+  public static String tradeIn = "You now have the option to trade in 3 compatible risk cards for armies";
   public static String tradeInTip = "";
+  public static String forcedTrade = "You have 5 risk cards: you have to trade in 3 to continue";
+  public static String alliance = "You can form alliances with fellow players to form a powerful union";
+  public static String allianceTip = "Form alliances to face stronger opponents, but be wary: your alliance partner can turn against you at any time";
   
 }
