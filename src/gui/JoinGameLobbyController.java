fff05d3253fdb670971acf99a639e489afebc990
Sandeep Kaur <skaur@mail.uni-mannheim.de>
2019-05-01 15:03:46 +0200

    #9 delete unused classes, attributes and methods in network package

--- src/gui/JoinGameLobbyController.java
+++ src/gui/JoinGameLobbyController.java
@@ -1,28 +1,19 @@
 package gui;
 
-
-import java.util.ArrayList;
-import game.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import main.Main;
-import network.client.Client;
 import network.messages.SendChatMessageMessage;
 
 
 public class JoinGameLobbyController {
 
-  @FXML
-  private Color x1;
   @FXML
   private TextField textField;
 
@@ -31,23 +22,15 @@ public class JoinGameLobbyController {
 
   @FXML
   private TextArea button;
-  
-  @FXML 
+
+  @FXML
   private Button leaveGame;
-  
-  @FXML CheckBox clientPlayer;
-  @FXML CheckBox hostBox;
-  @FXML CheckBox client2;
-  @FXML CheckBox client3;
-  @FXML CheckBox client4;
-  @FXML CheckBox client5;
-  ArrayList<CheckBox> clientBoxes = new ArrayList<CheckBox>();
-  
+
   @FXML
   void handleSendMessage(ActionEvent event) {
     String message = textField.getText();
-    SendChatMessageMessage m = new SendChatMessageMessage(
-    ProfileSelectionGUIController.selectedPlayerName, message);
+    SendChatMessageMessage m =
+        new SendChatMessageMessage(ProfileSelectionGUIController.selectedPlayerName, message);
     NetworkController.gameFinder.getClient().sendMessage(m);
   }
 
@@ -73,20 +56,5 @@ public class JoinGameLobbyController {
     button.setStyle("-fx-text-fill:black; -fx-font-size: 15px;");
     button.appendText(content + "\n");
 
-  }
-
-  /**
-   * @skaur
-   */
-  public void initialize() {  
-    clientBoxes = new ArrayList<CheckBox>();
-    clientBoxes .add(hostBox);
-    clientBoxes .add(clientPlayer);
-    clientBoxes .add(client2);
-    clientBoxes .add(client3);
-    clientBoxes .add(client4);
-    clientBoxes .add(client5);
-    hostBox.setSelected(true);
-    clientPlayer.setSelected(true);
   }
 }
