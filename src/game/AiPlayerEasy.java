1540bd790a782a7a633651eca320e7b1185f266d
Simon Metzger <smetzger@mail.uni-mannheim.de>
2019-05-03 09:29:21 +0200

    #34 edited style and added comments

--- src/game/AiPlayerEasy.java
+++ src/game/AiPlayerEasy.java
@@ -1,59 +1,61 @@
 package game;
 
-import java.util.ArrayList;
 import java.util.Vector;
 import main.Main;
 
 /**
+ * Class defines an easy ai player which acts completely random.
+ * 
  * @author smetzger
  * @author pcoberge
  *
  */
 public class AiPlayerEasy extends Player implements AiPlayer {
 
+  /**
+   * Constructor which calls the super constructor of the player class to set the name, the color
+   * and the game of the player. It also adds the created player to the player list.
+   */
   public AiPlayerEasy() {
     super(AiPlayerNames.getRandomName(), PlayerColor.values()[Main.g.getPlayers().size()], Main.g);
     Main.g.addAiNames(this.getName());
   }
 
   /**
-   * @author smetzger
-   * @author pcoberge
+   * Method represents the acting of the Ai in the first phase. It computes a random number between
+   * one and 42 which represents a random territory the ai places an army on
    */
   public synchronized void initialTerritoryDistribution() {
     int random = 0;
     do {
       random = (random != 0 ? (random % 42) + 1 : (int) (Math.random() * 42) + 1);
-      System.out.print(random + " ");
     } while (!super.initialTerritoryDistribution(Main.g.getWorld().getTerritories().get(random)));
     if (!Main.g.isNetworkGame()) {
       Main.b.updateLabelTerritory(Main.g.getWorld().getTerritories().get(random));
-
       Main.b.updateColorTerritory(Main.g.getWorld().getTerritories().get(random));
-      System.out
-          .println(Main.g.getCurrentPlayer().getName() + "--" + Main.g.getCurrentPlayer().getColor()
-              + "--" + Main.g.getWorld().getTerritories().get(random));
-      // System.out.println(Main.g.getCurrentPlayer().getColor() + " method");
-      // System.out.println(Main.g.getWorld().getTerritories().get(random));
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
       Main.g.furtherInitialTerritoryDistribution();
     }
   }
 
+  /**
+   * Method represents the acting of the AI in the second phase. It selects one of the territory the
+   * Ai owns to distribute an army of the remaining armies on.
+   */
   public synchronized void initialArmyDistribution() {
     int random = 0;
     Territory territory = null;
+    // while the armyDistribution method of the player class returns false it computes a new random
+    // number to select a territory
     do {
       random = (int) (Math.random() * this.getTerritories().size()) + 1;
       int i = 1;
       for (Territory t : this.getTerritories()) {
         if (i == random) {
-          System.out.println(t.getName());
           territory = t;
           break;
         } else {
@@ -68,33 +70,41 @@ public class AiPlayerEasy extends Player implements AiPlayer {
 
   }
 
+  /**
+   * Method represents the acting of the AI in the third phase. The Ai trades in their risk cards as
+   * soon as possible It places a random number of armies on a random selected territory
+   */
+
   public synchronized void armyDistribution() {
-    System.out.println("AI army distribution: " + Main.g.getCurrentPlayer().getName());
-    // trade-in cards
+    /** trade cards **/
+    // checks if the player owns at least three risk cards
     if (this.getCards().size() >= 3) {
+      // going through all possible combinations of cards
       for (int i = 0; i < this.getCards().size(); i++) {
         for (int j = i + 1; j < this.getCards().size() - 1; j++) {
           for (int k = j + 1; k < this.getCards().size() - 2; k++) {
+            // if possible the three cards will be traded in
             this.tradeCards(this.getCards().get(i), this.getCards().get(j), this.getCards().get(k));
           }
         }
       }
     }
 
+    /** select territory **/
     int randomTerritory = 0;
     int randomNumberOfArmies = 0;
     Territory territory = null;
-    // choose territory
+
     while (this.getNumberArmiesToDistibute() != 0) {
-      // System.out.println(this.getNumberArmiesToDistibute());
       do {
+        // random number between one and the amount of territories he owns
         randomTerritory = (int) (Math.random() * this.getTerritories().size()) + 1;
+        // random number between one and the amount of armies he can distribute
         randomNumberOfArmies = (int) (Math.random() * this.getNumberArmiesToDistibute()) + 1;
         int i = 1;
         for (Territory t : this.getTerritories()) {
           if (i == randomTerritory) {
             territory = t;
-            System.out.println(territory.getName() + " -- " + randomNumberOfArmies);
             break;
           } else {
             i++;
@@ -108,20 +118,22 @@ public class AiPlayerEasy extends Player implements AiPlayer {
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
-      System.out.println(this.getNumberArmiesToDistibute());
     }
-    System.out.println("not in schleife");
+    // sets the game state to the next stage and calls the attack method
     Main.g.setGameState(GameState.ATTACK);
     this.attack();
 
   }
 
+  /**
+   * Method represents the acting of the AI in the fourth phase. It selects a random territory from
+   * which the attack starts, a random territory to attack and a random number of armies to attack
+   * with
+   */
   public synchronized void attack() {
-    System.out.println(
-        "AI attack: " + Main.g.getCurrentPlayer().getName() + " -- " + this.getColor().toString());
+
     int randomTerritoryOwn = 0;
     int randomTerritoryOpponent = 0;
     int randomNumberOfArmies = 0;
@@ -133,12 +145,15 @@ public class AiPlayerEasy extends Player implements AiPlayer {
 
     double random = Math.random();
 
+    // checks if an attack is even possible
     while (this.isCapableToAttack() && random > attackProbability) {
       // choose own territory that is able to attack
-      System.out.println("Random1: " + random);
       do {
+        // random number between zero and the amount of own territories
         randomTerritoryOwn = (int) (Math.random() * this.getTerritories().size());
+        // selects a random territory to start the attack from with the computed random number above
         territoryOwn = this.getTerritoriesArrayList().get(randomTerritoryOwn);
+        // random number of armies to attack with
         randomNumberOfArmies = (int) (Math.random() * territoryOwn.getNumberOfArmies() - 1) + 1;
       } while (territoryOwn.getNumberOfArmies() == 1
           || territoryOwn.getHostileNeighbor().size() == 0);
@@ -160,48 +175,47 @@ public class AiPlayerEasy extends Player implements AiPlayer {
       // attack chosen territory and update GUI
       if (super.attack(attackerDices, defenderDices, territoryOwn, territoryOpponent,
           randomNumberOfArmies)) {
-        System.out.println(territoryOwn.getName() + " -- " + territoryOpponent.getName());
         Main.b.updateLabelTerritory(territoryOwn);
         Main.b.updateLabelTerritory(territoryOpponent);
         Main.b.updateColorTerritory(territoryOpponent);
-        System.out.println("UPDATE");
       } else {
         Main.b.updateLabelTerritory(territoryOwn);
         Main.b.updateLabelTerritory(territoryOpponent);
         try {
           Thread.sleep(1000);
         } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
           e.printStackTrace();
         }
-        System.out.println(territoryOwn.getName() + " -- " + territoryOpponent.getName());
       }
       attackProbability = 0.66;
       random = Math.random();
-      System.out.println("Random2: " + random);
     }
-
-    // Main.b.handleSkipGameState();
+    // sets the game state to the next stage
     Main.g.setGameState(GameState.FORTIFY);
     try {
       Thread.sleep(1000);
     } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    System.out.println("next Step");
+    // calls the fortify method of the class
     this.fortify();
 
   }
 
+  /**
+   * Method represents the acting of the AI in the last phase. It just skips the fortify stage and
+   * passes on to the next player
+   */
   public synchronized void fortify() {
-    System.out.println("AI fortify: " + Main.g.getCurrentPlayer());
-    // Main.b.handleSkipGameState();
-    // if (!Main.g.isNetworkGame()) {
     Main.g.furtherFortify();
-    // }
   }
 
+  /**
+   * Method checks if the Ai player is able to attack, means it has more than one army on its chosen
+   * territory.
+   * 
+   * @return true or false regarding to its number of armies on the selected territory
+   */
   public boolean isCapableToAttack() {
     for (Territory t : this.getTerritories()) {
       if (t.getNumberOfArmies() > 1) {
